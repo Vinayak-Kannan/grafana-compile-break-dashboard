25580e916e2c31c9760245adf3f3a778839598d3
Vinayak-Kannan <37912249+Vinayak-Kannan@users.noreply.github.com>
2025-03-21 20:03:53 +0000

    test

--- parse_dynamo_explain.py
+++ parse_dynamo_explain.py
@@ -1,7 +1,10 @@
 def find_graph_breaks_line(file_path):
+	print("here")
 	with open(file_path, 'r') as file:
 		for line in file:
+			print(line)
 			if 'Graph Breaks' in line:
+				print("LOOK HERE")
 				print(line)
 				break
 
